cd095d29b409baf1a373a20c195ac44b93184ab0
Shlynz <75536038+shlynz@users.noreply.github.com>
2023-11-18 17:50:06 +0100

    Basic implementation of ProbabilityDistribution for Die

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,49 @@ pub struct Die {
     mean: f64,
     median: f64,
 }
+
+impl ProbabilityDistribution for Die {
+    fn add(&self, probability_distribution: impl ProbabilityDistribution) -> Die {
+        unimplemented!()
+    }
+
+    fn add_dependent<F>(&self, callback_fn: F) -> Die
+    where
+        F: Fn(&i32) -> Die,
+    {
+        unimplemented!()
+    }
+
+    fn get_chance(&self, value: i32) -> f64 {
+        println!("{:?}", value);
+        unimplemented!()
+    }
+
+    fn get_result(&self) -> Vec<i32> {
+        self.values.clone()
+    }
+
+    fn get_min(&self) -> i32 {
+        self.min
+    }
+
+    fn get_max(&self) -> i32 {
+        self.max
+    }
+
+    fn get_mean(&self) -> f64 {
+        self.mean
+    }
+
+    fn get_median(&self) -> f64 {
+        self.median
+    }
+
+    fn get_variance(&self) -> f64 {
+        self.variance
+    }
+
+    fn get_standard_deviation(&self) -> f64 {
+        self.standard_deviation
+    }
+}

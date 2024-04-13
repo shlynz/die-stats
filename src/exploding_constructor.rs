4a5c3ab287adc171699b9f4e09017048dcf0690e
shlynz <75536038+shlynz@users.noreply.github.com>
2024-04-13 14:50:52 +0200

    Generalized exploding constructor to allow for either end to explode

--- src/exploding_constructor.rs
+++ src/exploding_constructor.rs
@@ -1,42 +1,83 @@
 use crate::{Die, Probability};
+use std::cmp::Ordering;
 
 pub trait ExplodingConstructor<V, P> {
-    fn new_exploding(sides: V, exploding_range: V, exploding_die: P) -> P;
-    fn exploding_from_range(start: V, end: V, exploding_range: V, exploding_die: P) -> P;
-    fn exploding_from_values(values: &[V], exploding_range: V, exploding_die: P) -> P;
+    fn new_exploding(
+        sides: V,
+        exploding_range: V,
+        exploding_condition: Ordering,
+        exploding_die: P,
+    ) -> P;
+    fn exploding_from_range(
+        start: V,
+        end: V,
+        exploding_range: V,
+        exploding_condition: Ordering,
+        exploding_die: P,
+    ) -> P;
+    fn exploding_from_values(
+        values: &[V],
+        exploding_range: V,
+        exploding_condition: Ordering,
+        exploding_die: P,
+    ) -> P;
     fn exploding_from_probabilities(
         probabilities: Vec<Probability<V>>,
         exploding_range: V,
+        exploding_condition: Ordering,
         exploding_die: P,
     ) -> P;
 }
 
 impl ExplodingConstructor<i32, Die> for Die {
-    fn new_exploding(sides: i32, exploding_range: i32, exploding_die: Die) -> Die {
-        Die::new(sides) + exploding_die_helper(exploding_range, exploding_die)
+    fn new_exploding(
+        sides: i32,
+        exploding_range: i32,
+        exploding_condition: Ordering,
+        exploding_die: Die,
+    ) -> Die {
+        Die::new(sides) + exploding_die_helper(exploding_range, exploding_condition, exploding_die)
     }
 
-    fn exploding_from_range(start: i32, end: i32, exploding_range: i32, exploding_die: Die) -> Die {
-        Die::from_range(start, end) + exploding_die_helper(exploding_range, exploding_die)
+    fn exploding_from_range(
+        start: i32,
+        end: i32,
+        exploding_range: i32,
+        exploding_condition: Ordering,
+        exploding_die: Die,
+    ) -> Die {
+        Die::from_range(start, end)
+            + exploding_die_helper(exploding_range, exploding_condition, exploding_die)
     }
 
-    fn exploding_from_values(values: &[i32], exploding_range: i32, exploding_die: Die) -> Die {
-        Die::from_values(values) + exploding_die_helper(exploding_range, exploding_die)
+    fn exploding_from_values(
+        values: &[i32],
+        exploding_range: i32,
+        exploding_condition: Ordering,
+        exploding_die: Die,
+    ) -> Die {
+        Die::from_values(values)
+            + exploding_die_helper(exploding_range, exploding_condition, exploding_die)
     }
 
     fn exploding_from_probabilities(
         probabilities: Vec<Probability<i32>>,
         exploding_range: i32,
+        exploding_condition: Ordering,
         exploding_die: Die,
     ) -> Die {
         Die::from_probabilities(probabilities)
-            + exploding_die_helper(exploding_range, exploding_die)
+            + exploding_die_helper(exploding_range, exploding_condition, exploding_die)
     }
 }
 
-fn exploding_die_helper(exploding_range: i32, exploding_die: Die) -> Box<dyn Fn(&i32) -> Die> {
+fn exploding_die_helper(
+    exploding_range: i32,
+    exploding_condition: Ordering,
+    exploding_die: Die,
+) -> Box<dyn Fn(&i32) -> Die> {
     Box::new(move |&prob: &_| {
-        if prob <= exploding_range {
+        if prob.cmp(&exploding_range) == exploding_condition {
             exploding_die.clone()
         } else {
             Die::empty()
@@ -48,6 +89,7 @@ fn exploding_die_helper(exploding_range: i32, exploding_die: Die) -> Box<dyn Fn(
 mod tests {
     use super::*;
     use crate::{Die, ProbabilityDistribution};
+    use std::cmp::Ordering;
 
     #[test]
     fn exploding_constructor() {
@@ -62,20 +104,26 @@ mod tests {
             },
         ];
         assert_eq!(
-            *Die::new_exploding(2, 1, Die::new(2)).get_probabilities(),
+            *Die::new_exploding(2, 2, Ordering::Less, Die::new(2)).get_probabilities(),
             expected_probabilities
         );
         assert_eq!(
-            Die::exploding_from_values(&vec![1, 2], 1, Die::new(2)).get_probabilities(),
-            &expected_probabilities
-        );
-        assert_eq!(
-            Die::exploding_from_probabilities(expected_probabilities.clone(), 1, Die::new(2))
+            Die::exploding_from_values(&vec![1, 2], 2, Ordering::Less, Die::new(2))
                 .get_probabilities(),
             &expected_probabilities
         );
         assert_eq!(
-            Die::exploding_from_range(1, 2, 1, Die::new(2)).get_probabilities(),
+            Die::exploding_from_probabilities(
+                expected_probabilities.clone(),
+                2,
+                Ordering::Less,
+                Die::new(2)
+            )
+            .get_probabilities(),
+            &expected_probabilities
+        );
+        assert_eq!(
+            Die::exploding_from_range(1, 2, 2, Ordering::Less, Die::new(2)).get_probabilities(),
             &expected_probabilities
         );
     }

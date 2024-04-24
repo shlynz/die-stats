dc6743999fa781f19edcbe20a015217ab0ec76dd
shlynz <75536038+shlynz@users.noreply.github.com>
2024-04-24 17:03:45 +0200

    Renames propagated up to file name

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 pub use crate::{
     die::Die,
-    drop_constructor::{DropInitializer, DropType},
-    exploding_constructor::{ExplodingCondition, ExplodingInitializer},
+    drop_initializer::{DropInitializer, DropType},
+    exploding_initializer::{ExplodingCondition, ExplodingInitializer},
     probability::Probability,
     probability_distribution::{ProbabilityDistribution, ProbabilityIter},
 };
 
 mod common;
 mod die;
-mod drop_constructor;
-mod exploding_constructor;
+mod drop_initializer;
+mod exploding_initializer;
 mod probability;
 mod probability_distribution;
